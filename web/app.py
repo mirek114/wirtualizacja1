a858ab3b18781b24cc76124cc6bf586a17c3b18d
mirek <mirek114@gmail.com>
2018-05-12 09:17:55 +0000

    web

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,3 +9,18 @@ def hello():
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
+
+@app.route("/order-animation")
+def order_animation():
+  return render_template(
+    "order.html",
+    invitation="siema"
+  )
+
+@app.route("/upload")
+def upload():
+  if 'my_file' not in request.files:
+    return "nie pyklo"
+  upl_file = request.files['my_file']
+  storage = new S3Storage(os.get, s3)
+  storage.save(path="", body=upl_file)

85cec6b94c9d601168122d7f56ac496ebb92077d
mirek <mirek114@gmail.com>
2018-05-12 08:18:04 +0000

    Part2

--- tests/test_s3_media_storage.py
+++ tests/test_s3_media_storage.py
@@ -1,4 +1,5 @@
 import unittest
+import os, boto3
 from media.s3_storage import S3MediaStorage 
 class TestS3Storage(unittest.TestCase):
 
@@ -16,7 +17,13 @@ class TestS3Storage(unittest.TestCase):
     assert storage.contains(path='my/test/path.txt')
   
   def there_is_s3_storage(self):
-    return S3MediaStorage()
+    s3 = boto3.resources('s3')
+    bucket_name = os.getenv('APP_BUVKET_NAME')
+    return S3MediaStorage(s3, bucket_name)
   
   def there_is_file(self):
-    pass
+    my_file = open('/tmp/test.txt', 'w')
+    my_file.write('Cos tam')
+    my_file.close()
+
+    return open('tmp/test.txt', 'rb')
